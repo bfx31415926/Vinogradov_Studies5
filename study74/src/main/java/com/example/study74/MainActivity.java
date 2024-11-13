a5610e555e84d9bbc056e64514c483f9d39812d4
bfx31415926 <smi-alexey@yandex.ru>
2024-11-13 17:13:29 +0300

    Добавил 74 урок

---
+++ study74/src/main/java/com/example/study74/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.study74;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem mi = menu.add(0, 1, 0, "Preferences");
+        mi.setIntent(new Intent(this, PrefActivity.class));
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
