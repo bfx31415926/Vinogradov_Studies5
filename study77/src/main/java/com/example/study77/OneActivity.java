9d561d18f8c479c6e33c2481f1004b14ff1d93b9
bfx31415926 <smi-alexey@yandex.ru>
2024-11-14 16:58:39 +0300

    Добавил 77 урок

---
+++ study77/src/main/java/com/example/study77/OneActivity.java
@@ -0,0 +1,12 @@
+package com.example.study77;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OneActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
+}
---
+++ study77/src/main/java/com/example/study77/TwoActivity.java
@@ -0,0 +1,12 @@
+package com.example.study77;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TwoActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
