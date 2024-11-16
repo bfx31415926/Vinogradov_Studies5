95ca2159c3ddcd91cd9790664082dc59d257f6f8
bfx31415926 <smi-alexey@yandex.ru>
2024-11-16 23:56:23 +0300

    Добавил 92 урок

---
+++ study92/src/main/java/com/example/study92/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.study92;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+    public void onClickStart(View v) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStop(View v) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
