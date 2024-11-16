76fc7a8e78feb11ab077746ea4b2fa699b755aa6
bfx31415926 <smi-alexey@yandex.ru>
2024-11-16 18:28:53 +0300

    Добавил 88 урок

---
+++ study88/src/main/java/com/example/study88/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.study88;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    MyTask mt;
+    TextView tvInfo;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+    }
+
+    public void onclick(View v) {
+        if (v.getId() == R.id.btnStart) {
+            mt = new MyTask();
+            mt.execute();
+        } else if (v.getId() == R.id.btnGet) {
+            showResult();
+        }
+    }
+
+/* 1-ый вариант
+    private void showResult() {
+        if (mt == null) return;
+        int result = -1;
+        try {
+            Log.d(LOG_TAG, "Try to get result");
+            result = mt.get();
+            Log.d(LOG_TAG, "get returns " + result);
+            Toast.makeText(this, "get returns " + result, Toast.LENGTH_LONG).show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+ */
+    //    /* 2-ый вариант
+    private void showResult() {
+        if (mt == null) return;
+        int result = -1;
+        try {
+            Log.d(LOG_TAG, "Try to get result");
+            result = mt.get(1, TimeUnit.SECONDS);
+            Log.d(LOG_TAG, "get returns " + result);
+            Toast.makeText(this, "get returns " + result, Toast.LENGTH_LONG).show();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            Log.d(LOG_TAG, "get timeout, result = " + result);
+            e.printStackTrace();
+        }
+    }
+//    */
+
+    class MyTask extends AsyncTask<Void, Void, Integer> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvInfo.setText("Begin");
+            Log.d(LOG_TAG, "Begin");
+        }
+
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 100500;
+        }
+
+        @Override
+        protected void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            tvInfo.setText("End. Result = " + result);
+            Log.d(LOG_TAG, "End. Result = " + result);
+
+        }
+    }
+}
